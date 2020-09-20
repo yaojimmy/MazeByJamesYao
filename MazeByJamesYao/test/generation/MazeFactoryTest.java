1d5d6c119df978682aeccd76071a1eb61da36e93
JamesYaoWM <jtyao@email.wm.edu>
2020-09-20 15:01:56 -0400

    Created MazeFactoryTest.java file

---
+++ MazeByJamesYao/test/generation/MazeFactoryTest.java
@@ -0,0 +1,14 @@
+package generation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MazeFactoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
