5078b2fa28ab6c7f955b1825390212fba891d43f
JamesYaoWM <jtyao@email.wm.edu>
2020-10-05 14:04:54 -0400

    Implemented all Wizard methods except algorithm

--- MazeByJamesYao/src/gui/Wizard.java
+++ MazeByJamesYao/src/gui/Wizard.java
@@ -3,41 +3,66 @@ package gui;
 import generation.Maze;
 
 public class Wizard implements RobotDriver {
+	Robot rob;
+	Maze m;
+	float energy_consumed;
+	int pathlength;
 
+	// sets robot
 	@Override
 	public void setRobot(Robot r) {
-		// TODO Auto-generated method stub
-
+		rob = r;
+		energy_consumed = 0;
+		pathlength = 0;
 	}
 
+	// sets maze
 	@Override
 	public void setMaze(Maze maze) {
-		// TODO Auto-generated method stub
+		m = maze;
 
 	}
 
+	/*
+	 * drives towards exit
+	 * if makes it to exit, returns true
+	 * if stops for whatever reason, returns false
+	 */
 	@Override
 	public boolean drive2Exit() throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		while(!rob.isAtExit()) {
+			if(!drive1Step2Exit())
+				return false;
+		}
+		return true;
 	}
 
+	// moves robot one step towards the exit according to algorithm
+	// returns true if it moves, false if it doesn't
 	@Override
 	public boolean drive1Step2Exit() throws Exception {
-		// TODO Auto-generated method stub
+		return wizard_alg();
+	}
+	
+	/*
+	 * wizard algorithm
+	 * use Maze information to find neighbor close to exit
+	 * can use sensors to determine where walls are
+	 */
+	private boolean wizard_alg() {
 		return false;
 	}
 
+	// returns total energy consumed
 	@Override
 	public float getEnergyConsumption() {
-		// TODO Auto-generated method stub
-		return 0;
+		return energy_consumed;
 	}
 
+	// returns total number of cells traversed
 	@Override
 	public int getPathLength() {
-		// TODO Auto-generated method stub
-		return 0;
+		return pathlength;
 	}
 
 }
