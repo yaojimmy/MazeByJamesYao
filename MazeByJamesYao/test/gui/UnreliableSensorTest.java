cec34255a132c857f858a4eba063041e71f1e4b0
JamesYaoWM <jtyao@email.wm.edu>
2020-10-11 16:37:30 -0400

    Pseudocode for UnreliableSensorTest

---
+++ MazeByJamesYao/test/gui/UnreliableSensorTest.java
@@ -0,0 +1,55 @@
+package gui;
+
+import org.junit.jupiter.api.Test;
+
+public class UnreliableSensorTest {
+
+	/**
+	 * tests setMaze method
+	 */
+	@Test
+	void testSetMaze() {
+		
+	}
+	
+	/**
+	 * tests setSensorDirection method
+	 */
+	@Test
+	void testSetSensorDirection() {
+		
+	}
+	
+	/**
+	 * tests getEnergyConsumptionForSensing method
+	 */
+	@Test
+	void testGetEnergyConsumptionForSensing() {
+		
+	}
+	
+	/**
+	 * tests distanceToObstacle method
+	 */
+	@Test
+	void testDistanceToObstacle() {
+		
+	}
+	
+	/**
+	 * tests startFailureAndRepairProcess method
+	 */
+	@Test
+	void testStartFailureAndRepairProcess() {
+		
+	}
+	
+	/**
+	 * tests stopFailureAndRepairProcess method
+	 */
+	@Test
+	void testStopFailureAndRepairProcess() {
+		
+	}
+	
+}
