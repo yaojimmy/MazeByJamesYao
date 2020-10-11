0136157835916e6a59a3c28741a318e17bfe0619
JamesYaoWM <jtyao@email.wm.edu>
2020-10-11 16:42:35 -0400

    Pseudocode for UnreliableRobotTest

---
+++ MazeByJamesYao/test/gui/UnreliableRobotTest.java
@@ -0,0 +1,39 @@
+package gui;
+
+import org.junit.jupiter.api.Test;
+
+public class UnreliableRobotTest {
+
+	/**
+	 * tests setting Controller
+	 */
+	@Test
+	void testSetController() {
+		
+	}
+	
+	/**
+	 * tests setting sensors
+	 */
+	@Test
+	void testSetSensor() {
+		
+	}
+	
+	/**
+	 * tests startFailureAndRepairProcess method
+	 */
+	@Test
+	void testStartFailureAndRepairProcess() {
+		
+	}
+	
+	/**
+	 * tests stopFailureAndRepairProcess method
+	 */
+	@Test
+	void testStopFailureAndRepairProcess() {
+		
+	}
+
+}
