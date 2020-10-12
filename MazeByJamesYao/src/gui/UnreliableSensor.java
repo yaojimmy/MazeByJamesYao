cd9425b6bbc8f71205e9a21adcac5aac42b39f6b
JamesYaoWM <jtyao@email.wm.edu>
2020-10-12 15:00:41 -0400

    Implemented UnreliableSensor

--- MazeByJamesYao/src/gui/UnreliableSensor.java
+++ MazeByJamesYao/src/gui/UnreliableSensor.java
@@ -1,5 +1,7 @@
 package gui;
 
+import generation.CardinalDirection;
+
 /**
  * Extends all functionality of ReliableSensor, with an added failure and repair process
  * Only can use method if not in operational state
@@ -9,6 +11,8 @@ package gui;
  */
 public class UnreliableSensor extends ReliableSensor implements Runnable, DistanceSensor {
 	private boolean operational;
+	private int timeToRepair;
+	private int timeBetweenFailures;
 
 	public UnreliableSensor() {
 		// starts in operational state
@@ -33,27 +37,45 @@ public class UnreliableSensor extends ReliableSensor implements Runnable, Distan
 	}
 	
 	/**
+	 * @return returns distance to obstacle if sensor has not failed
+	 */
+	public int distanceToObstacle(int[] currentPosition, CardinalDirection currentDirection, float[] powersupply)
+			throws Exception {
+		if (operational) {
+			return super.distanceToObstacle(currentPosition, currentDirection, powersupply);
+		}
+		else {
+			throw new Exception("sensorfailure");
+		}
+	}
+	/**
 	 * goes through failure and repair process
 	 * @param meanTimeBetweenFailures time between failures
 	 * @param meanTimeToRepair time before sensor becomes operational again
 	 */
 	public void startFailureAndRepairProcess(int meanTimeBetweenFailures, int meanTimeToRepair)
 			throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-
+		Thread failureRepair = new Thread(this);
+		operational = true;
+		timeToRepair = meanTimeToRepair;
+		timeBetweenFailures = meanTimeBetweenFailures;
+		failureRepair.start();
 	}
 	
 	/**
 	 * ends failure and repair process
 	 */
 	public void stopFailureAndRepairProcess() throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-
+		operational = true;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		operational = false;
+		try {
+			Thread.sleep(timeToRepair*1000);
+		} catch (InterruptedException e) {}
+		stopFailureAndRepairProcess();
 		
 	}
 
